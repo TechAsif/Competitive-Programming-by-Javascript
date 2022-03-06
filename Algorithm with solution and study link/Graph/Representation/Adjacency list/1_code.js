c3c0c105d4fe30c74e08eae138b925b913e8d376
Tech Asif <asif.letsgobd@gmail.com>
2022-03-06 15:55:52 +0600

    unweighted graph adjacency list Reprejentation is done

--- Algorithm with solution and study link/Graph/Representation/Adjacency list/1_code.js
+++ Algorithm with solution and study link/Graph/Representation/Adjacency list/1_code.js
@@ -1,42 +1,74 @@
-// Javascript code to demonstrate Graph representation
-// using ArrayList in Java
+class Graph {
 
-// A utility function to add an edge in an
-// undirected graph
-function addEdge(adj, u, v) {
-  adj[u].push(v);
-  adj[v].push(u);
-}
+  constructor(noOfVertices) {
 
-// A utility function to print the adjacency list
-// representation of graph
-function printGraph(adj) {
-  for (let i = 0; i < adj.length; i++) {
-    console.log("\nAdjacency list of vertex" + i + " \n");
-    console.log("head");
-    for (let j = 0; j < adj[i].length; j++) {
-      console.log(" -> " + adj[i][j]);
-    }
-    console.log("\n");
+    this.noOfVertices = noOfVertices;
+    this.adjList = new Map();
+
   }
+
+  //Add Vetext into map key and initiliaze map key value in empty array
+  addVertex(v) {
+    this.adjList.set(v, [])
+  }
+
+  addEdge(v, w) {
+
+    this.adjList.get(v).push(w);
+
+    //if graph is undirected then put w vertex to v edge connection also
+
+    this.adjList.get(w).push(v);
+
+  }
+
+  printGraph() {
+
+    //get the key of adjacency list that means a vertex
+    let getKeys = this.adjList.keys();
+
+    for (let i of getKeys) {
+      let getValues = this.adjList.get(i);
+
+      let concateSameAdjdata = '';
+
+      for (let j of getValues) {
+        concateSameAdjdata += j + " ";
+      }
+
+      console.log(i+ '->'+concateSameAdjdata);
+
+    }
+
+
+  }
+
+
 }
 
-// Driver Code
-// Creating a graph with 5 vertices
-let V = 5;
-let adj = [];
 
-for (let i = 0; i < V; i++) {
-  adj.push([]);
+let graph = new Graph(6);
+
+let vertices = ["A", "B", "C", "D", "E", "F"];
+
+//Adding Vertices
+for(i = 0; i<vertices.length; i++){
+  graph.addVertex(vertices[i]);
 }
 
-// Adding edges one by one
-addEdge(adj, 0, 1);
-addEdge(adj, 0, 4);
-addEdge(adj, 1, 2);
-addEdge(adj, 1, 3);
-addEdge(adj, 1, 4);
-addEdge(adj, 2, 3);
-addEdge(adj, 3, 4);
+//Adding Edges
+graph.addEdge("A",'B')
+graph.addEdge("A",'D')
+graph.addEdge("A",'E')
+graph.addEdge("B",'C')
+graph.addEdge("D",'E')
+graph.addEdge("E",'F')
+graph.addEdge("E",'C')
+graph.addEdge("C",'F')
 
-printGraph(adj);
+//Print The Graph
+graph.printGraph();
+
+
+
+
